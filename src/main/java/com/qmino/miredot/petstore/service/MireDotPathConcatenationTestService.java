64ac1536911b10f23f372857020633a40436c0ca
Jeroen Vuerinckx <jeroen.vuerinckx@qmino.com>
2013-05-23 16:45:59 +0200

    Adds tests for path concatenation

---
+++ src/main/java/com/qmino/miredot/petstore/service/MireDotPathConcatenationTestService.java
@@ -0,0 +1,49 @@
+package com.qmino.miredot.petstore.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * <p/>
+ * <p>
+ * <i>Creation-Date</i>: 23/05/13<br>
+ * <i>Creation-Time</i>:  12:17<br>
+ * </p>
+ *
+ * @author Jeroen
+ * @author <a href="http://www.qmino.com">Qmino bvba</a>
+ */
+@Path("test")
+public interface MireDotPathConcatenationTestService {
+    /**
+     * Testing the path concatenation
+     * @summary Testing the path concatenation
+     * @return Nothing meaningful.
+     */
+    @GET
+    @Path("path/concatenation")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testingPathConcatenation();
+
+    /**
+     * Testing the path concatenation with trailing slash
+     * @summary Testing the path concatenation
+     * @return Nothing meaningful.
+     */
+    @GET
+    @Path("path/concatenation/trailing/")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testingPathConcatenation2();
+
+    /**
+     * Testing the path concatenation with starting slash
+     * @summary Testing the path concatenation
+     * @return Nothing meaningful.
+     */
+    @GET
+    @Path("/path/concatenation/starting")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testingPathConcatenation3();
+}
