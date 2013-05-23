5900e386f15105c10c6627617a8e7cb4532be649
Jeroen Vuerinckx <jeroen.vuerinckx@qmino.com>
2013-05-23 16:45:59 +0200

    Adds tests for path concatenation

---
+++ src/main/java/com/qmino/miredot/petstore/service/MireDotPathNotOnInterfaceTestService.java
@@ -0,0 +1,28 @@
+package com.qmino.miredot.petstore.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * <p/>
+ * <p>
+ * <i>Creation-Date</i>: 23/05/13<br>
+ * <i>Creation-Time</i>:  12:17<br>
+ * </p>
+ *
+ * @author Jeroen
+ * @author <a href="http://www.qmino.com">Qmino bvba</a>
+ */
+public interface MireDotPathNotOnInterfaceTestService {
+    /**
+     * This method should be documented, although the interface has no @Path annotation.
+     * @summary Test @Path on method, but not on interface
+     * @return Nothing meaningful.
+     */
+    @GET
+    @Path("/test/path/notoninterface")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testingPathNotOnInterface();
+}
