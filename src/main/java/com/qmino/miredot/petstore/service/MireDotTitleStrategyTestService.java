607751e389c3b0caf7d11b73e1e9d3035676151e
Jeroen Vuerinckx <jeroen.vuerinckx@qmino.com>
2013-07-31 11:39:11 +0200

    Adds title strategy example

---
+++ src/main/java/com/qmino/miredot/petstore/service/MireDotTitleStrategyTestService.java
@@ -0,0 +1,54 @@
+package com.qmino.miredot.petstore.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * <p/>
+ * <p>
+ * <i>Creation-Date</i>: 31/07/13<br>
+ * <i>Creation-Time</i>:  11:07<br>
+ * </p>
+ *
+ * @author Jeroen
+ * @author <a href="http://www.qmino.com">Qmino bvba</a>
+ */
+@Path("test/title/")
+public interface MireDotTitleStrategyTestService {
+
+    /**
+     * The first sentence will be used as the title of this interface.
+     * <title>
+     *     <strategy>firstsentence</strategy>
+     * </title>
+     * @return A number
+     */
+    @Path("firstsentence")
+    @GET
+    public int firstSentence();
+
+    /**
+     * The content from the @title tag will be the title of this interface.
+     * <title>
+     *     <strategy>tag: title</strategy>
+     * </title>
+     * @title The title from the @title tag.
+     * @return A number
+     */
+    @Path("titletag")
+    @GET
+    public int titleTag();
+
+    /**
+     * The content from the @summary tag will be the title of this interface.
+     * <title>
+     *     <strategy>tag: summary</strategy>
+     * </title>
+     * @title The title from the @title tag
+     * @summary The title from the @summary tag.
+     * @return A number
+     */
+    @Path("summarytag")
+    @GET
+    public int summaryTag();
+}
