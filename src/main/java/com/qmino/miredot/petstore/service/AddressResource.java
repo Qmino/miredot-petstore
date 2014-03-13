e1a30d104ec629511b3c354216219e8cab1607c8
Jeroen Vuerinckx <jeroen.vuerinckx@qmino.com>
2014-03-13 12:20:43 +0100

    Removes non-standard javadoc

---
+++ src/main/java/com/qmino/miredot/petstore/service/AddressResource.java
@@ -0,0 +1,14 @@
+package com.qmino.miredot.petstore.service;
+
+/**
+ * <p/>
+ * <p>
+ * <i>Creation-Date</i>: 28/02/14<br>
+ * <i>Creation-Time</i>:  11:41<br>
+ * </p>
+ *
+ * @author Jeroen
+ * @author <a href="http://www.qmino.com">Qmino bvba</a>
+ */
+public class AddressResource {
+}
