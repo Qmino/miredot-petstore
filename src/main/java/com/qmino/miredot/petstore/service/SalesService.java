fadd5efeea27e875dc5d145e686698261d6ef0d6
Yves Vandewoude <yves.vandewoude@qmino.com>
2015-01-22 17:07:50 +0100

    Added example data for bean/form parameters

--- src/main/java/com/qmino/miredot/petstore/service/SalesService.java
+++ src/main/java/com/qmino/miredot/petstore/service/SalesService.java
@@ -1,6 +1,8 @@
 package com.qmino.miredot.petstore.service;
 
+import com.qmino.miredot.petstore.domain.Credentials;
 import com.qmino.miredot.petstore.domain.Order;
+import org.jboss.resteasy.annotations.Form;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -22,30 +24,33 @@ public interface SalesService {
      * Get the orders of a customer
      *
      * @param customerId The id of the customer to get the orders for
+     * @param credentials The credentials of the caller
      * @return All orders of the customer with specified id
      * @summary Get the orders of a customer
      */
     @GET
     @Path("/order")
     @Produces({MediaType.APPLICATION_JSON})
-    public Order[] getOrders(@QueryParam("customerId") Long customerId);
+    public Order[] getOrders(@QueryParam("customerId") Long customerId, @BeanParam Credentials credentials);
 
     /**
      * Get the order with the specified id
      *
      * @param orderId The order id
+     * @param credentials The credentials of the caller
      * @return The order with the specified id
      * @summary Get the order with the specified id
      */
     @GET
     @Path("/order/{id}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Order getOrder(@PathParam("id") Long orderId);
+    public Order getOrder(@PathParam("id") Long orderId, @BeanParam Credentials credentials);
 
     /**
      * Get the status of the order with specified id
      *
      * @param orderId The order id
+     * @param credentials The credentials of the caller
      * @return The status of the order with specified id
      * @summary Get the order status
      * @servicetag status
@@ -53,5 +58,5 @@ public interface SalesService {
     @GET
     @Path("/order/{id}/status")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getOrderStatus(@PathParam("id") Long orderId);
+    public String getOrderStatus(@PathParam("id") Long orderId, @Form Credentials credentials);
 }
